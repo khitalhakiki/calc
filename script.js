afe2afdc498cf3a7741ad76a3e5618969e580f99
khitalhakikidev <61872086+khitalhakiki@users.noreply.github.com>
2020-07-14 18:25:14 +0700

    Add files via upload
    javascript perhitungan calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+ var inputLabel = document.getElementById('inputLabel');
+     
+    function tekanBtn(obj) {
+         
+        var pushed = obj.innerHTML;
+         
+        if (pushed == '=') {
+            // Calculate
+            inputLabel.innerHTML = eval(inputLabel.innerHTML);
+             
+        } else if (pushed == 'AC') {
+            // All Clear
+            inputLabel.innerHTML = '0';
+             
+        } else {
+            if (inputLabel.innerHTML == '0') {
+                inputLabel.innerHTML = pushed;
+                 
+            } else {
+                inputLabel.innerHTML += pushed;   
+            }
+        }
+    }
